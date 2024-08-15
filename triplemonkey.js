9eb5f08204c694b7d57a6766033c1b460f309c2b
TripleMonkey <triplemonkeystudio@gmail.com>
2024-08-15 16:22:07 -0400

    ADD: add email contact functionality via formsubmit.co

---
+++ triplemonkey.js
@@ -0,0 +1,15 @@
+/*
+    TripleMonkey Studio 
+    Javascript for html
+*/
+
+
+
+// const LINK_ICON_SIZE = "30%";
+// document.getElementById('icon').innerHTML = LINK_ICON_SIZE;
+// document.getSelection('size').innerHTML = LINK_ICON_SIZE
+
+window.onload = function(){
+    const LINK_ICON_SIZE = "30%";
+    document.getElementById('icon').innerHTML = LINK_ICON_SIZE;
+};
