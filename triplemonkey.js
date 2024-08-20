56f810ecd5cad3559a889577ec87b4e0be2c2db8
TripleMonkey <triplemonkeystudio@gmail.com>
2024-08-20 18:09:06 -0400

    ACC: remove duplicate menu button from tab order to improve accessibility

--- triplemonkey.js
+++ triplemonkey.js
@@ -9,7 +9,13 @@
 // document.getElementById('icon').innerHTML = LINK_ICON_SIZE;
 // document.getSelection('size').innerHTML = LINK_ICON_SIZE
 
+
+const LINK_ICON_SIZE = "30px";
+
 window.onload = function(){
-    const LINK_ICON_SIZE = "30%";
-    document.getElementById('icon').innerHTML = LINK_ICON_SIZE;
+    document.getElementById('icon').style.width = LINK_ICON_SIZE;
+};
+
+window.onload = function(){
+
 };
